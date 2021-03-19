790b026eb83838b39371a2a0c8af8b27fd83b5e0
Taito <taito.tanaka1211@gmail.com>
2021-03-19 23:31:52 +0900

    ngrok用ブランチ

--- MorphoMusic/settings.py
+++ MorphoMusic/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.ngrok.io', 'localhost']
 
 
 # Application definition
