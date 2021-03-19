32c7e56da32b2b23c177cc70c025e4742e0fc8c2
aleeca <49315610+aleeca@users.noreply.github.com>
2021-03-19 13:57:44 +0900

    Working on LINE SDKでリクエストのIDトークンを検証する #2

--- api/urls.py
+++ api/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('api/', views.IndexView.as_view()),
+    path('', views.get_data),
 ]
--- api/views.py
+++ api/views.py
@@ -1,3 +1,39 @@
+from django.conf import settings
 from django.shortcuts import render
+from django.views.decorators.http import require_GET
+from django.http import JsonResponse
+import requests
+import json
 
-# Create your views here.
+
+@require_GET
+def get_data(request):
+    res_body, result = verify(request.headers.get("Idtoken"))
+    if result == "success":
+        res_dict = json.loads(res_body)
+        user_id = res_dict["sub"]
+        print(user_id)
+        data = {"status": "success"}
+        return JsonResponse(get_songs(data))
+
+    elif result == "failed":
+        res_dict = json.loads(res_body)
+        return JsonResponse({"status": "failed", "description": res_dict["error_description"]})
+
+    else:  # noqa
+        return JsonResponse({"status": "failed"})
+
+
+def get_songs(data):
+    return data
+
+
+def verify(token):
+    # LINE IDトークンの検証
+    url = "https://api.line.me/oauth2/v2.1/verify"
+    payload = {"id_token": token, "client_id": str(settings.LIFF_CHANNEL_ID)}
+    response = requests.post(url, data=payload)
+    if response.status_code == 200:
+        return response.text, "success"
+    else:
+        return response.text, "failed"
