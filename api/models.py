9da7e2b8653c763ed317814d4a878594ec20df6b
aleeca <49315610+aleeca@users.noreply.github.com>
2021-03-19 16:30:17 +0900

    created_dateカラムを追加

--- api/models.py
+++ api/models.py
@@ -1,10 +1,14 @@
 from django.db import models
+import datetime
+
 
 class Lineuser(models.Model):
     user_id = models.CharField(max_length=256)
+
 
 class Song(models.Model):
     line_user = models.ForeignKey(Lineuser, on_delete=models.CASCADE)
     song_name = models.CharField(max_length=128)
     artist_name = models.CharField(max_length=128)
     artwork_url = models.URLField()
+    created_date = models.DateTimeField(default=datetime.datetime.now)
