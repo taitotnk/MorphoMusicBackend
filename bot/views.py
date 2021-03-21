d8cd9b6e9cd2866e1de6396908644e0bafb484d1
aleeca <49315610+aleeca@users.noreply.github.com>
2021-03-21 13:18:45 +0900

    test

--- bot/views.py
+++ bot/views.py
@@ -122,13 +122,6 @@ def handle_song_message(event):
                 alt_text="test",
                 contents={
                     'type': 'bubble',
-                    'hero': {
-                        'type': 'image',
-                        'url': 'https://is3-ssl.mzstatic.com/image/thumb/Music128/v4/bf/b6/3a/bfb63ad9-36a7-99a7-ab40-c8a4c1efb91d/source/100x100bb.jpg',
-                        'size': 'full',
-                        'aspectRatio': '20:13',
-                        'aspectMode': 'cover'
-                    },
                     'body': {
                         'type': 'box',
                         'layout': 'vertical',
@@ -153,21 +146,10 @@ def handle_song_message(event):
                         'spacing': 'sm',
                         'contents': [
                             {
-                                'type': 'button',
-                                'style': 'primary',
-                                'height': 'sm',
-                                'action': {
-                                    'type': 'uri',
-                                    'label': 'URL',
-                                    'uri': 'https://music.apple.com/jp/album/こんにちは/1440793037?i=1440793289&amp;uo=4'
-                                }
-                            },
-                            {
                                 'type': 'spacer',
                                 'size': 'sm'
                             }
                         ],
-                        'flex': 0
                     }
                 }
             ))
